97add500d3dfe8de9844c64d8e4c8a59f0ec097d
Yashwant Lalwani <yashwant.lalwani@gmail.com>
2023-09-22 15:58:47 +0530

    Added Categories

--- components/Catalog.jsx
+++ components/Catalog.jsx
@@ -7,11 +7,14 @@ import {
   CardHeader,
   CardFooter,
   Image,
+  Select,
+  SelectItem,
 } from "@nextui-org/react";
 import Link from "next/link";
 
 const Catalog = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [selectedGenre, setSelectedGenre] = useState(""); // State for selected genre filter
   const [books, setBooks] = useState([
     {
       id: 1,
@@ -26,8 +29,8 @@ const Catalog = () => {
       id: 2,
       title: "Sample Book 2",
       author: "Author 2",
-      img: "https://edit.org/images/cat/book-covers-big-2019101610.jpg",
-      genre: "Non-fiction",
+      img: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/yellow-business-leadership-book-cover-design-template-dce2f5568638ad4643ccb9e725e5d6ff_screen.jpg?ts=1637017516",
+      genre: "Fiction",
       publicationDate: "2021-07-20",
       isFree: false,
     },
@@ -36,7 +39,7 @@ const Catalog = () => {
       title: "Sample Book 2",
       author: "Author 2",
       img: "https://edit.org/images/cat/book-covers-big-2019101610.jpg",
-      genre: "Non-fiction",
+      genre: "Fiction",
       publicationDate: "2021-07-20",
       isFree: true,
     },
@@ -45,7 +48,7 @@ const Catalog = () => {
       title: "Sample Book 2",
       author: "Author 2",
       img: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/yellow-business-leadership-book-cover-design-template-dce2f5568638ad4643ccb9e725e5d6ff_screen.jpg?ts=1637017516",
-      genre: "Non-fiction",
+      genre: "Horror",
       publicationDate: "2021-07-20",
       isFree: false,
     },
@@ -66,68 +69,93 @@ const Catalog = () => {
     // Update the 'books' state with the fetched data
   };
 
+  const handleGenreChange = (genre) => {
+    setSelectedGenre(genre);
+  };
+
+  // Filter books based on the selected genre
+  const filteredBooks = selectedGenre
+    ? books.filter((book) => book.genre === selectedGenre)
+    : books;
+
+  const uniqueGenres = [...new Set(books.map((book) => book.genre))];
+
   return (
-    <div className="container mx-auto py-6">
-      <h1 className="text-3xl font-semibold mb-4">Book Catalog</h1>
-      <div className="flex">
+    <div className="container sm:px-10 py-6">
+      <h1 className="text-3xl font-semibold mb-8">Book Catalog</h1>
+      <div className="flex mb-4">
         <Input
           size="lg"
-          className="mb-2"
+          className="mr-2"
           placeholder="Search books by title or author"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <Button onClick={handleSearch} className="mb-4">
+        <Button onClick={handleSearch} className="mr-2">
           Search
         </Button>
+        <select
+          value={selectedGenre}
+          onChange={(e) => handleGenreChange(e.target.value)}
+          className="rounded-lg p-2 border border-gray-300 focus:outline-none focus:border-blue-400"
+        >
+          <option value="">All Genres</option>
+          {uniqueGenres.map((genre) => (
+            <option key={genre} value={genre}>
+              {genre}
+            </option>
+          ))}
+        </select>
       </div>
 
       <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 m-4">
-        {books.map((item, index) => (
-          <Card
-            shadow="sm"
-            key={index}
-            isPressable
-            onPress={() => console.log("item pressed")}
-            isFooterBlurred
-          >
-            <CardHeader className="absolute z-10 top-1 flex-col items-start">
-              <p className="text-tiny text-white/60 uppercase font-bold">
-                {item.publicationDate}
-              </p>
-              <h4 className="text-white/90 font-medium text-xl">
-                {item.title}
-              </h4>
-            </CardHeader>
-            <Image
-              isZoomed
-              removeWrapper
-              src={item.img}
-              alt={item.title}
-              className="z-0 w-full h-full object-cover"
-            />
-            <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-              <div className="flex flex-grow gap-2 items-center">
-                <Image
-                  className="rounded-full w-10 h-11 bg-black"
-                  src={item.img}
-                  alt={item.title}
-                />
-                <div className="grid grid-cols-2">
-                  <Link href={`/book/${item.id}`} key={item.id}>
-                    <p className="text-tiny text-white/60">{item.author}</p>
-                    <p className="text-tiny text-white/60">{item.price}</p>
-                  </Link>
+        {books
+          .filter((item) => !selectedGenre || item.genre === selectedGenre)
+          .map((item, index) => (
+            <Card
+              shadow="sm"
+              key={index}
+              isPressable
+              onPress={() => console.log("item pressed")}
+              isFooterBlurred
+            >
+              <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <p className="text-tiny text-white/60 uppercase font-bold">
+                  {item.publicationDate}
+                </p>
+                <h4 className="text-white/90 font-medium text-xl">
+                  {item.title}
+                </h4>
+              </CardHeader>
+              <Image
+                isZoomed
+                removeWrapper
+                src={item.img}
+                alt={item.title}
+                className="z-0 w-full h-full object-cover"
+              />
+              <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+                <div className="flex flex-grow gap-2 items-center">
+                  <Image
+                    className="rounded-full w-10 h-11 bg-black"
+                    src={item.img}
+                    alt={item.title}
+                  />
+                  <div className="grid grid-cols-2">
+                    <Link href={`/book/${item.id}`} key={item.id}>
+                      <p className="text-tiny text-white/60">{item.author}</p>
+                      <p className="text-tiny text-white/60">{item.price}</p>
+                    </Link>
+                  </div>
                 </div>
-              </div>
-              <Button radius="full" size="sm">
-                <Link href={`/book/${item.id}`} key={item.id}>
-                  Read Reviews
-                </Link>
-              </Button>
-            </CardFooter>
-          </Card>
-        ))}
+                <Button radius="full" size="sm">
+                  <Link href={`/book/${item.id}`} key={item.id}>
+                    Read Reviews
+                  </Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
       </div>
     </div>
   );
--- pages/Test.jsx
+++ pages/Test.jsx
@@ -1,57 +1,32 @@
 import React from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
+import { Button } from "@nextui-org/react";
 
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Image,
-  Button,
-} from "@nextui-org/react";
-
-export default function App() {
-  return (
-    <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
-      <Card
-        shadow="sm"
-        key={index}
-        isPressable
-        onPress={() => console.log("item pressed")}
-        isFooterBlurred
-        className="w-full h-[300px] col-span-12 sm:col-span-7"
-      >
-        <CardHeader className="absolute z-10 top-1 flex-col items-start">
-          <p className="text-tiny text-white/60 uppercase font-bold">
-            {item.publicationDate}
-          </p>
-          <h4 className="text-white/90 font-medium text-xl">Book Tite</h4>
-        </CardHeader>
-        <Image
-          isZoomed
-          removeWrapper
-          src={item.img}
-          alt={item.title}
-          className="z-0 w-full h-full object-cover"
-        />
-        <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-          <div className="flex flex-grow gap-2 items-center">
-            <Image
-              className="rounded-full w-10 h-11 bg-black"
-              src={item.img}
-              alt={item.title}
-            />
-            <div className="flex flex-col">
-              <Link href={`/book/${item.id}`} key={item.id}>
-                <p className="text-tiny text-white/60">{item.author}</p>
-                <p className="text-tiny text-white/60">{item.price}</p>
-              </Link>
+const Fullpage = () => (
+  <ReactFullpage
+    //fullpage options
+    scrollingSpeed={1000} /* Options here */
+    scrollHorizontally={true} /* Because we are using the extension */
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section bg-red-500">
+            <div className="text-center">
+              <p className="text-center text-8xl font-bold mb-3">
+                Book Review Website
+              </p>
+              <Button onClick={() => fullpageApi.moveSectionDown()}>
+                Click me to move down
+              </Button>
             </div>
           </div>
-          <Button radius="full" size="sm">
-            Read Reviews
-          </Button>
-        </CardFooter>
-      </Card>
-    </div>
-  );
-}
+          <div className="section bg-yellow-400">
+            <p className="text-center">Section 2</p>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
+
+export default Fullpage;
